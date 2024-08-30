8ca4fab5f6b0c3dc0aa51c1db7d77ebb23e90b48
Nitish G. Saji <nitishsaji2001@gmail.com>
2024-08-30 23:26:12 +0530

    Reponse 200 sent

--- app/server.go
+++ app/server.go
@@ -2,24 +2,56 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
-
 )
 
-func main (){
+func main() {
 
-	fmt.Println("Logs appear here!!");
-	
+	fmt.Println("Logs appear here!!")
+
 	l, err := net.Listen("tcp", "0.0.0.0:8080")
 	if err != nil {
 		fmt.Println("Failed to bind to port 8080")
 		os.Exit(1)
 	}
-
-	_, err = l.Accept()
+  for{
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	go handleConnection(conn)
+  }
+	
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	fmt.Println("New connection from:", conn.RemoteAddr())
+
+	//read request
+
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil && err != io.EOF {
+		fmt.Println("Error reading:", err)
+		return
+	}
+	fmt.Printf("Received request:\n%s\n", string(buffer[:n]))
+
+	//send response
+
+	response := "HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHelloWORLD!!"
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		fmt.Println("Error sending response")
+		return
+
+	}
+
+	fmt.Println("Response sent!")
+
 }
